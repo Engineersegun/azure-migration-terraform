e7b3eb0b1beeb8a74ea67367d43da9e92983c0dd
Ubuntu <azureuser@Azure-Source-Server.0nmbzj0z5ioe3c5ds4ew0yjlwd.xx.internal.cloudapp.net>
2026-02-21 11:08:06 +0000

    Confirmed clean deploy

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  default = "Migration-Lab-RG-v2"
+}
+
+variable "admin_password" {
+  description = "Password for the Azure Source VM"
+  default     = "Migration2026" 
+}
+
+s
